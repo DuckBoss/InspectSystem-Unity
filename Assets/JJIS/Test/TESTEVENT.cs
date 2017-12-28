5697596a91ac0bbb93832da479225ca6616b8279
Jason <DuckBoss@users.noreply.github.com>
2017-12-28 01:40:17 -0500

    Updated prefabs and included a test event
    TESTEVENT.cs is used for testing the unity events at the end of dialogues. Feel free to remove this for your projects.

---
+++ Assets/JJIS/Test/TESTEVENT.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JJIS {
+	public class TESTEVENT : MonoBehaviour {
+
+		public void TestEvent() {
+			Debug.Log("THIS IS A TEST EVENT FROM THE INSPECTION SYSTEM");
+		}
+
+	}
+}
